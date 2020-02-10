379af96f8db97bbcb356d61f2e6f174a1935f928
KevinR <k.rohn@outlook.de>
2020-02-10 14:22:03 +0000

    updates

--- if-listener.js
+++ if-listener.js
@@ -1,11 +1,40 @@
-module.exports = function(RED) {
+module.exports = function (RED) {
     function LowerCaseNode(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
         var node = this;
-        node.on('input', function(msg) {
-            msg.payload = msg.payload.toLowerCase();
+        let os = require('os');
+
+        // Track the listeners and provide a way of adding a new one
+        // You would probably want to put this into some kind of module
+        // so it can be used in various places
+        let listeners = [];
+        function onChange(f) {
+            if (listeners.indexOf(f) === -1) {
+                listeners.push(f);
+            }
+        }
+        let oldInterfaces = [];
+
+        node.on('input', function (msg) {
+            process.nextTick(function checkInterfaces() {
+                let interfaces = os.networkInterfaces();
+
+                // Quick and dirty way of checking for differences
+                // Not very efficient
+                if (JSON.stringify(interfaces) !== JSON.stringify(oldInterfaces)) {
+                    listeners.forEach((f) => f(interfaces));
+                    oldInterfaces = interfaces;
+                }
+
+                process.nextTick(checkInterfaces);
+            });
+            
+            var msgTxt = 'xx';
+            onChange(msgTxt);
+
+            msg.payload = msgTxt;
             node.send(msg);
         });
     }
-    RED.nodes.registerType("if-listener",LowerCaseNode);
+    RED.nodes.registerType("if-listener", LowerCaseNode);
 }
